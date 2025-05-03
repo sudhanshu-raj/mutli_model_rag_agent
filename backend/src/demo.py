44c63e20407f66aa94990891b8826355a37aac48
sudhanshu_raj <rajsudhanshu9431@gmail.com>
2025-05-04 00:52:41 +0530

    backend src demo clean

--- backend/src/demo.py
+++
@@ -1,8 +0,0 @@
-import os
-import config as cfg
-from dotenv import load_dotenv
-load_dotenv()
-origin="http://localhost:5173"  # Replace with the actual origin you want to check
-
-allowed_origin = origin if origin in cfg.FRONTEND_ORIGINS else cfg.FRONTEND_ORIGINS
-print(allowed_origin)  # This will print the allowed origin based on the condition
